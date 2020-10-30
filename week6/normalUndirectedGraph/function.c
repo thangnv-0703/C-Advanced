c748c6f99bfb12d76d22f5181656022c30882e13
ngoclp2000 <nindaphi@gmail.com>
2020-10-31 01:44:58 +0700

    Completed Week 6
    =)) tired

---
+++ week6/normalUndirectedGraph/function.c
@@ -0,0 +1,42 @@
+#include "data.h"
+#include "function.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+Graph createGraph(int sizemax){
+    Graph newGraph;
+    newGraph.sizemax = sizemax;
+    newGraph.matrix = (int*) malloc(sizeof(int)*sizemax*sizemax);
+    memset(newGraph.matrix,0,sizeof(int)*sizemax*sizemax);
+    return newGraph;
+}
+
+void addEdge(Graph graph,int v1,int v2){
+    graph.matrix[v1*graph.sizemax+v2] = 1;
+    graph.matrix[v2*graph.sizemax+v1] = 1;
+}
+int adjacent(Graph graph,int v1,int v2){
+    return graph.matrix[v1*graph.sizemax + v2];
+}
+int getAdjacentVertices(Graph graph,int vertex,int *output){
+    int n = 0;
+    int j = 0;
+    for(int i = 0 ; i < graph.sizemax ; i++){
+        if(graph.matrix[vertex*graph.sizemax + i]){
+            *(output + j++) = vertex*graph.sizemax + i;
+            n++;
+        }
+    }
+    return n;
+}
+void dropGraph(Graph graph){
+    for(int i = 0 ; i < graph.sizemax*graph.sizemax ; i++)
+        free(graph.matrix + i);
+}
+void printGraph(Graph graph){
+    for(int i = 0 ; i < graph.sizemax ; i++){
+        for(int j = 0 ; j < graph.sizemax ; j++)
+            printf("%d ",*(graph.matrix + (i*graph.sizemax + j)));
+        printf("\n");
+    }
+}
--- week6/normalUndirectedGraph/function.h
+++ week6/normalUndirectedGraph/function.h
@@ -8,5 +8,5 @@ void addEdge(Graph graph,int v1,int v2);
 int adjacent(Graph graph,int v1,int v2);
 int getAdjacentVertices(Graph graph,int vertex,int *output);
 void dropGraph(Graph graph);
-
+void printGraph(Graph Graph);
 #endif
